5246b05202d98960bd2dc5fa3ed3c6d0b6f76c64
Muhammadsodiq <xikmatullayevmuhammadsodiq21@gmail.com>
2026-04-01 16:14:00 +0500

    asigfrfs

--- src/config/settings.py
+++ src/config/settings.py
@@ -14,8 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="django-insecure-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env("DEBUG"))
